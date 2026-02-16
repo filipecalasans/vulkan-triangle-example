b31aa0c1e13d4d13707ce011086b8405023dd64c
Filipe Calasans <filipe.calasans@gmail.com>
2026-02-15 22:31:04 -0800

    Fix out of order struct field initizliation warning

--- lve_model.cpp
+++ lve_model.cpp
@@ -132,29 +132,29 @@ std::vector<VkVertexInputAttributeDescription>  LveModel::Vertex::getAttributeDe
     std::vector<VkVertexInputAttributeDescription> attributeDescriptions(4);
     // Vertes
     attributeDescriptions[0] = {
-        .binding = 0,
         .location = 0,
+        .binding = 0,
         .format = VK_FORMAT_R32G32B32_SFLOAT,
         .offset = offsetof(Vertex, position),
     };
     // Color
     attributeDescriptions[1] = {
-        .binding = 0,
         .location = 1,
+        .binding = 0,
         .format = VK_FORMAT_R32G32B32_SFLOAT,
         .offset = offsetof(Vertex, color),
     };
     // Lighting (nomals)
     attributeDescriptions[2] = {
-        .binding = 0,
         .location = 2,
+        .binding = 0,
         .format = VK_FORMAT_R32G32B32_SFLOAT,
         .offset = offsetof(Vertex, normal),
     };
     // Textures (uv)
     attributeDescriptions[3] = {
-        .binding = 0,
         .location = 3,
+        .binding = 0,
         .format = VK_FORMAT_R32G32_SFLOAT,
         .offset = offsetof(Vertex, uv),
     };
